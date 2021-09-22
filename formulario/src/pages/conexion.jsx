f74d7bf80a8d702268fdc37fc7d338097569e996
Mauricio Lombano <mauricio.lombano@gmai.com>
2021-09-22 13:39:43 -0500

    Ajustes varios

--- formulario/src/pages/conexion.jsx
+++ formulario/src/pages/conexion.jsx
@@ -8,7 +8,7 @@ import { Redirect } from 'react-router';
 
 import { Col, Row, Container } from 'react-bootstrap'
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import './conexion.css'
 
@@ -27,14 +27,14 @@ function Main() {
 
     return (
         <Container className="containerMain">
+            <img src="https://quickpak.com.mx/wp-content/uploads/2021/06/WhatsApp-Image-2021-06-16-at-11.02.49-AM-1.jpeg" className="imgBg" />
             <Row>
                 <Col xs={3}>
-                    <Card sx={{ minWidth: 275 }}>
+                    <Card sx={{ minWidth: 275 }} className="contCard">
                         <CardContent>
-
-                            <Container className="text-center">
-                                <Row className="mt-1">
-                                    Bienvenido a nuestro servicio web.
+                            <Container className="text-cenr cardContentCont">
+                                <Row className="mt-1 ">
+                                    <h4 class="textBienvenida">Bienvenido a nuestro servicio web.</h4>
                                 </Row>
                                 <Row className="mt-2">
                                     <Fab variant="extended" onClick={showOrHidePass}>
@@ -49,7 +49,7 @@ function Main() {
                                 </Row>
                                 <Row className="mt-2">
                                     <Fab variant="extended">
-                                        <Link to ="/user" className="noLinkStyle">
+                                        <Link to="/user" className="noLinkStyle">
                                             Soy Administrador
                                         </Link>
                                     </Fab>
